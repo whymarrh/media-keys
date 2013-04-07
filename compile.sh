1dcf03aec1b1f3fdd53c3068b09cd811e174979c
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-04-07 01:55:34 -0230

    Errors occured with spaces in the paths.

--- compile.sh
+++ compile.sh
@@ -8,17 +8,17 @@ LWS_INCLUDE_DIR=$LWS_DYNAMIC_LIB_DIR/include
 LWS_DYNAMIC_LIB=$LWS_DYNAMIC_LIB_DIR/lib/libwebsockets.dylib
 
 # build libwebsockets if it can't be found
-if [ ! -f $LWS_DYNAMIC_LIB ]; then
-	echo "libwebsockets.dylib was not found in "$LWS_DYNAMIC_LIB". Building it."
+if [ ! -f "$LWS_DYNAMIC_LIB" ]; then
+	echo "libwebsockets.dylib was not found in $LWS_DYNAMIC_LIB. Building it."
 	mkdir -p build
 	cd build
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$LWS_DYNAMIC_LIB_DIR ../lib/libwebsockets
+	cmake -DCMAKE_INSTALL_PREFIX:"PATH=$LWS_DYNAMIC_LIB_DIR" ../lib/libwebsockets
 	make
 	make install
 	cd ..
 fi
 
 # this is how ObjC is compiled from the command line?
-clang -fobjc-arc -framework Cocoa -o $SERVER_EXE $SERVER_SOURCE -I $LWS_INCLUDE_DIR $LWS_DYNAMIC_LIB
+clang -fobjc-arc -framework Cocoa -o "$SERVER_EXE" "$SERVER_SOURCE" -I "$LWS_INCLUDE_DIR" "$LWS_DYNAMIC_LIB"
 
 echo "All done."
